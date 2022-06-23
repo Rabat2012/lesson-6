99b260fb4ac1e31a3cf886054c43ebfb772a0c8b
Rick <rabat.imankulov2001@gmail.com>
2022-06-23 11:50:22 +0600

    new

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,33 @@
+import { IconButton } from "@mui/material";
+import React, { useContext } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { counterContext } from "../../contexts/counterContext";
+
+const Counter = () => {
+  const { counter, decrement } = useContext(counterContext);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <IconButton
+        onClick={() => decrement()}
+        aria-label="delete"
+        color="primary">
+        <RemoveIcon />
+      </IconButton>
+      <span>{counter}</span>
+      <IconButton
+        onClick={() => decrement()}
+        aria-label="delete"
+        color="primary">
+        <AddIcon />
+      </IconButton>
+    </div>
+  );
+};
+
+export default Counter;
